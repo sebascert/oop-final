6c3230de0b1ce8317ec77566e9b9b6239219f2c6
YoAquinJS <sebascertuche@gmail.com>
2024-06-09 21:16:23 -0600

    Added video rater class

---
+++ classes/video_rater.cpp
@@ -0,0 +1,39 @@
+#include "video_rater.hpp"
+
+#include <algorithm>
+
+VideoRater::VideoRater(std::istringstream input){
+    //TODO
+}
+
+VideoRater::~VideoRater(){
+    for(Video* vid : videos)
+        delete vid;
+}
+
+std::vector<Video*> VideoRater::getVideosBy(std::function<bool(Video*)> predicate) const{
+    std::vector<Video*> foundVideos;
+    std::copy_if(videos.begin(), videos.end(), std::back_inserter(foundVideos), predicate);
+    return foundVideos;
+}
+
+std::vector<Video*> VideoRater::getCatalog() const{
+    return videos;
+}
+
+std::vector<Video*> VideoRater::getByRate(float minRate, int videoTypes) const{
+    return getVideosBy([minRate](Video* v) {
+        return v->getRating()>=minRate;
+    });
+}
+
+std::vector<Video*> VideoRater::getByGenre(const Genre& genre, int videoTypes) const{
+    return getVideosBy([genre](Video* v) {
+        return v->getGenre() == genre;
+    });
+}
+
+Video* VideoRater::operator[](const Id& id){
+    auto found = std::find_if(videos.begin(), videos.end(), [id](Video* v){return v->hasId(id);});
+    return found != videos.end() ? *found : nullptr;
+}
---
+++ classes/video_rater.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <vector>
+#include <sstream>
+#include <functional>
+
+#include "video.hpp"
+
+class VideoRater{
+    std::vector<Video*> videos;
+
+    std::vector<Video*> getVideosBy(std::function<bool(Video*)> predicate) const;
+
+    public:
+        VideoRater(std::istringstream input);
+        ~VideoRater();
+
+        std::vector<Video*> getCatalog() const;
+        std::vector<Video*> getByRate(float minRate, int videoTypes) const;
+        std::vector<Video*> getByGenre(const Genre& genre, int videoTypes) const;
+        Video* operator[](const Id& id);
+};
