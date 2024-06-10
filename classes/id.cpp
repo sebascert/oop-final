fa4c983d5e424efc310215ec0b72992496eceb62
YoAquinJS <sebascertuche@gmail.com>
2024-06-09 18:37:00 -0600

    Included invalid arg except

--- classes/id.cpp
+++ classes/id.cpp
@@ -9,15 +9,19 @@ const int Id::episodeLength = 2;
 
 
 Id::Id(std::string id) {
-    int p = 0;
-    this->id = std::stoi(id.substr(p, Id::idLength));
-    if (id.length() == Id::idLength)
-        return;
-
-    p += Id::idLength+2;
-    this->season = std::stoi(id.substr(p, Id::seasonLength));
-    p += Id::seasonLength+1;
-    this->episode = std::stoi(id.substr(p, Id::episodeLength));
+    if (id.length() == Id::idLength){
+        this->id = std::stoi(id.substr(0, Id::idLength));
+        this->season = 0;
+        this->episode = 0;
+    }else if (id.length() == Id::idLength+Id::seasonLength+Id::episodeLength+3){
+        int p = 0;
+        this->id = std::stoi(id.substr(p, Id::idLength));
+        p += Id::idLength+2;
+        this->season = std::stoi(id.substr(p, Id::seasonLength));
+        p += Id::seasonLength+1;
+        this->episode = std::stoi(id.substr(p, Id::episodeLength));
+    }else
+        throw std::invalid_argument("Id invalida");
 }
 
 std::string Id::toString() const{
@@ -26,7 +30,7 @@ std::string Id::toString() const{
     if (season != 0)
         ss << "-S" << std::setw(Id::seasonLength) << std::setfill('0') << +season;
     if (episode != 0)
-        ss << "-E" << std::setw(Id::episodeLength) << std::setfill('0') << +episode;
+        ss << "E" << std::setw(Id::episodeLength) << std::setfill('0') << +episode;
 
     return ss.str();
 }
